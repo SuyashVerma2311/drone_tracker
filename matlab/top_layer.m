1d5d9d1d871a787bde96eb02e1651635d2442985
SuyashVerma2311 <suchiv2311@gmail.com>
2023-01-26 18:57:42 +0530

    oh, we back.

--- matlab/top_layer.m
+++ matlab/top_layer.m
@@ -27,3 +27,23 @@
 
 
 %% 9. Go back to 2nd step
+
+
+
+
+%% Plotting Below:
+
+t = -6:0.01:6;
+
+figure(1);
+grid on; hold on; view(3);
+plot3(0.*t+5,t,0.*t,'b');
+plot3(0.*t-5,t,0.*t,'b');
+plot3(t,0.*t+5,0.*t,'b');
+plot3(t,0.*t-5,0.*t,'b');
+fill3([-6,6,6,-6],[-6,-6,6,6],[0,0,0,0],'g');
+alpha(0.3);
+cams = stem3(0,0,0,'--blacksquare','filled');
+set(cams,'xdata',[-5,-5,5,5],'ydata',[-5,5,-5,5],'zdata',[2,2,2,2]);
+zlim([0 3]); xlim([-6 6]); ylim([-6 6]);
+
